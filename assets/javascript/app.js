7744376ff51ac51d7340f24d247bd75e8871cbef
Dania King <dania@fortunebuilders.com>
2020-03-03 20:51:34 -0800

    more front end

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// need the following global variables Train name, Destination, Fequency in minutes, next arrival time, and minutes away
+// load the page with a few existing routes
+// will need precreated train routes in firebase that load when the page loads
+// calculte time?
+
+var trains = {
+    trainName: "trainName",
+    destination: "destination",
+    frequency: 'frequency',
+    nextArrival: 'nextArrival',
+    minutesAway: 'minutesAway'
+
+}
+
